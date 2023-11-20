dd7bb2683be284f85b3ed9f33cf791c37399b1d9
BlueBeard <desaeger@hcri.com>
2023-11-19 18:03:01 -0800

    Created a second branch

---
+++ Mohamed-GitHub/MainMenu/MainMenuSelection.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenuSelection.swift
+//  Mohamed-GitHub
+//
+//  Created by Robert De Saeger on 11/19/23.
+//
+
+import SwiftUI
+
+struct MainMenuSelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMenuSelection()
+}
